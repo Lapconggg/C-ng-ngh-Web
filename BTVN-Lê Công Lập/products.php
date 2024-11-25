85bae4c2075b61d188d3acb83158e1ee20d4361a
Lapconggg <leconglap94@gmail.com>
2024-11-26 00:34:40 +0700

    Add files via upload

---
+++ BTVN-Lê Công Lập/products.php
@@ -0,0 +1,15 @@
+<?php
+$products = array (
+  0 => 
+  array (
+    'name' => 'mũ ',
+    'price' => '100000',
+    'image' => 'images/h1.webp',
+  ),
+  1 => 
+  array (
+    'name' => 'quân',
+    'price' => '100000',
+    'image' => 'images/h2.webp',
+  ),
+);
